c10a5ea50117451bb8ce9eac3262aeb3bd6c632a
MarkMelior <mark.melior@yandex.ru>
2023-12-09 00:03:03 +0300

    upd

---
+++ CodeWars/6kyu/2. Find The Parity Outlier.js
@@ -0,0 +1,15 @@
+// You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+
+// Examples
+// [2, 4, 0, 100, 4, 11, 2602, 36] -->  11 (the only odd number)
+
+// [160, 3, 1719, 19, 11, 13, -21] --> 160 (the only even number)
+
+function findOutlier(integers) {
+	const arr = integers.map(el => (el % 2 ? el : null))
+	if (arr.filter(el => el === null).length === 1) {
+		return integers[arr.indexOf(null)]
+	} else {
+		return arr.find(el => el !== null)
+	}
+}
